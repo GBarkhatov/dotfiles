56242af648ed5df6f4a295e9cd480e545c48584c
Govard Barkhatov <gbarkhatov@gmail.com>
2016-01-22 20:01:07 +1000

    rm shit, changed terminato's opacity

--- .config/i3/randomwall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/gbarkhatov/Pictures/Wallpapers
-while [[ true ]]; do
-	#statements
-	randomPicture=$(ls | sort -R | head -n 1)
-	feh --bg-scale "${randomPicture}"
-	sleep 1800 
-done
